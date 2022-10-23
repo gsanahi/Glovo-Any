d9a62d1480de312eb6ec5a5e5e6e3a389ab7ede2
Anahi Garcia <gsanahi@gmail.com>
2022-10-23 22:19:54 +0200

    def word probability

--- Alice’s Adventures in Wonderland/Alice.py
+++ Alice’s Adventures in Wonderland/Alice.py
@@ -18,12 +18,12 @@ def remove_stopwords(txt,wordsToRemove): #list[str], set[stopwords] -> Newlist [
 
 #print(remove_stopwords(alice,stopwords_set))
 
-names = ['hdb--','Annie Aa', 'Hola.', 'John Psuffixes$', ] 
+names = ['hdb—','Annie Aa', 'Hola.', 'John Psuffixes$', ] 
 
-suffixes = set(punctuation)
+suffixes = set(punctuation + "— ")
 
 def remove_punctuation(palabra):#string ->string
-    if palabra[-1] in suffixes:
+    if (palabra != "") and (palabra[-1] in suffixes):
         return remove_punctuation(palabra[0:-1])
     else:
         return palabra
@@ -51,4 +51,24 @@ def normalize(lst): #List[cadenas] -> List[cadenas]
     without_punctuation = map(lambda x : remove_punctuation(x),without_stopwords)
     without_apostrophe = map(lambda x : remove_apostrophe(x),without_punctuation)
     return list(without_apostrophe)
+
+def count_words(lst): #List -> dict
+    dict = {}
+    # Comprobar si esa palabra ya está en el diccionario
+    for palabra in lst:
+        if palabra in dict:
+            # Si lo está, incrementa su valor (el contado) en una unidad
+            dict[palabra] = dict[palabra] + 1
+        # Si no lo está, métela con el valor de 1    
+        else:
+            dict[palabra] = 1
+    return dict
     
+#print(count_words(normalize(alice)))  
+
+
+def word_probability(wordsCount):
+    size = float(len(wordsCount))
+    return dict(map(lambda tupla : (tupla[0],tupla[1]/size),wordsCount.items()))
+    
+print(word_probability(count_words(normalize(alice))))
