69a98cd9c2a427f39dfdca5fb5a6f2d0c987c567
Anahi Garcia <gsanahi@gmail.com>
2022-11-22 10:10:55 -0300

    base

--- javasScript/secret number/secret.js
+++ javasScript/secret number/secret.js
@@ -1,15 +1,39 @@
-// Configuración de mínimo y máximo.
+import readline from 'readline';
 
-funtion calculateRandonNum (min,max) {
+// Configuración de mínimo y máximo.
+// Cálculo del número secreto.
+
+function calculateRandonNum (min,max) {
     const randomNum = Math.floor(Math.random() * (max-min + 1)) + min;
     return randomNum;
 }
 
+const secretNumber = calculateRandonNum(0,100);
+
+//Pedir número al usuario.
+
+console.log(secretNumber);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 
+function getNumberfromConsole(){
+    const promise = new promise((resolve, reject) => {       
+        rl.question('introduce el numero:', (num) => {
+            rl.pause();
+            resolve(num)
+        })
+    })
+    return promise;
+}
 
-Cálculo del número secreto.
-Pedir número al usuario.
-Comprobar si el número es el número secreto:
-Si lo es, tenemos ganador!
-Si no lo es, damos la pista
+const numberFromConsole = await getNumberfromConsole()
+
+console.log(numberFromConsole)
+
+//Comprobar si el número es el número secreto:
+//Si lo es, tenemos ganador!
+//Si no lo es, damos la pista
